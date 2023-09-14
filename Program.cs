a6b0e0ec1ce3553334ca57c20adf1c9d47de49ba
Марина Мельникова <melm2004@mail.ru>
2023-09-14 18:54:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Solver;
+
+double a = Convert.ToDouble(Console.ReadLine()),
+    b = Convert.ToDouble(Console.ReadLine()),
+    c = Convert.ToDouble(Console.ReadLine());
+
+var result = Solve.SolveEquation(a, b, c);
